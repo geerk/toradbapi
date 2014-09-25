a853e4821443a29f98bf5e94ec067897d80949f1
Timofey Trukhanov <timofey.trukhanov@gmail.com>
2014-09-25 20:08:44 +0300

    add setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='toradbapi',
+    version='0.1',
+    description='Wrapper for twisted.enterprise.adbapi.ConnectionPool to use with tornado',
+    long_description=open('README.rst').read(),
+    author='Timofey Trukhanov',
+    author_email='timofey.trukhanov@gmail.com',
+    license='MIT',
+    test_suite='tests',
+    install_requires=('tornado', 'twisted'),
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database'))
